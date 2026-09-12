43a4c76a808c2ac2228c1048de40d999ae6796b4
wisdomnova <wisdomdivine3d@gmail.com>
2026-09-12 13:18:37 +0100

    feat(auth): remove Sign in with Google and X from SignInModal

--- components/signin-modal.tsx
+++ components/signin-modal.tsx
@@ -113,34 +113,6 @@ export function SignInModal({ isOpen, onClose, redirectTo }: SignInModalProps) {
     }
   };
 
-  const handleGoogleOAuth = async () => {
-    setSelectedMethod("google");
-    setIsConnecting(true);
-    try {
-      // TODO: Implement Google OAuth
-      toast.error("Google OAuth coming soon");
-      setIsConnecting(false);
-      setSelectedMethod(null);
-    } catch {
-      setIsConnecting(false);
-      setSelectedMethod(null);
-    }
-  };
-
-  const handleXOAuth = async () => {
-    setSelectedMethod("x");
-    setIsConnecting(true);
-    try {
-      // TODO: Implement X OAuth
-      toast.error("X OAuth coming soon");
-      setIsConnecting(false);
-      setSelectedMethod(null);
-    } catch {
-      setIsConnecting(false);
-      setSelectedMethod(null);
-    }
-  };
-
   const authMethods = [
     {
       id: "demo",
@@ -165,45 +137,6 @@ export function SignInModal({ isOpen, onClose, redirectTo }: SignInModalProps) {
       onClick: handleBscSignIn,
       isPrimary: true,
       icon: <IconWallet className="w-5 h-5 text-[#F3BA2F]" />,
-    },
-    {
-      id: "google",
-      label: "Google Account",
-      description: "Continue with Google",
-      onClick: handleGoogleOAuth,
-      isPrimary: false,
-      icon: (
-        <svg viewBox="0 0 24 24" className="w-5 h-5">
-          <path
-            fill="#4285F4"
-            d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-          />
-          <path
-            fill="#34A853"
-            d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-          />
-          <path
-            fill="#FBBC05"
-            d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-          />
-          <path
-            fill="#EA4335"
-            d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-          />
-        </svg>
-      ),
-    },
-    {
-      id: "x",
-      label: "X / Twitter",
-      description: "Continue with X",
-      onClick: handleXOAuth,
-      isPrimary: false,
-      icon: (
-        <svg viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 text-white">
-          <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24h-6.627l-5.1-6.614L2.896 21.75H.588l7.753-8.835L0 2.25h6.791l4.6 6.088 5.253-6.088zM17.15 19.812h1.828L5.97 4.11H4.036l13.114 15.702z" />
-        </svg>
-      ),
     },
   ];
 
